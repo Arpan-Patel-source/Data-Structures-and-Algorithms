fe37995efe122f37fbe5781c6e671ca373889d56
Arpan <arpanpatel2408@gmail.com>
2026-09-24 22:51:02 +0530

    Solved Problem 41

---
+++ Leetcode_Problems/Hard/0041_First Missing Positive.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int firstMissingPositive(int[] nums)
+    {
+        int i, n = nums.length;
+        for(i = 0; i<n; i++)
+        {
+            while (nums[i] >= 1 && nums[i] <= n && nums[nums[i] - 1] != nums[i])
+            {
+                int index = nums[i]-1;
+                int temp = nums[i];
+                nums[i]=nums[index];
+                nums[index]=temp;
+            }
+        }
+        for(i=0;i<n;i++)
+            if(nums[i]!=i+1)return i+1;
+        return n+1;
+    }
+}
